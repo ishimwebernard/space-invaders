3e85d0b9e47bc0a7489997b73769e84d5128c009
“ishimwebernard” <isbernard2001@gmail.com>
2021-03-13 06:55:51 +0200

    mend

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,7 +2,7 @@ const Video = ({ video }) =>{
     console.log(video);
     return (
         <div className="video">
-            <img src={`${video.snippet.thumbnails.default.url}`} alt=""/>
+            <img src={`${video.snippet.thumbnails.high.url}`} alt=""/>
             <p className="title">{`${video.snippet.title}`}</p>
             <div className="date-set">
                 <p className="date">{`${video.snippet.publishedAt}`}</p>
